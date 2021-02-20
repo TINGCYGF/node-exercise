6b9ca7b56fd37c570f831cc7aa73691bafc9ca95
TINGCYGF <1127163160@qq.com>
2021-02-20 10:55:09 +0800

    添加404

--- node-server/index.ts
+++ node-server/index.ts
@@ -2,44 +2,47 @@ import * as http from "http";
 import {IncomingMessage, ServerResponse} from "http";
 import * as fs from "fs";
 import * as p from "path";
-import {publicDecrypt} from "crypto";
 
 const server = http.createServer();
+
 //拼接项目路径，__dirname为目录地址
 const publicDir = p.resolve(__dirname, 'public')
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
   //url重命名为path
-  const {method, url, headers} = request;
-  const path = request.headers.referer
-  console.log('path:' + path)
-  const myURL = new URL('http://localhost:8888/index.html');
-  console.log(myURL.pathname);
-  console.log(myURL.search);
+  console.log(request);
+  const {method, url: path, headers, rawHeaders} = request;
+  console.log(path + rawHeaders[1]);
+  const {pathname, search} = new URL(path, 'http://'+ rawHeaders[1])
 
-
-  switch (myURL.search) {
-    case '/index.html':
-      fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    case '/style.css':
-      response.setHeader('Content-type', 'text/css; charset=utf-8');
-      fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    case '/main.js':
-      response.setHeader('Content-type', 'text/css; charset=utf-8');
-      fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
+  if(method !== 'GET') {
+    response.statusCode = 200;
+    response.end('这是一个假响应');
+    return;
   }
+
+  let filename = pathname.substr(1);
+  if(filename === '') {
+    filename = 'index.html'
+  }
+  fs.readFile(p.resolve(publicDir, filename), (error, data) => {
+    if(error) {
+      if (error.errno === -4058){
+        response.statusCode = 404;
+        fs.readFile(p.resolve(publicDir, '404.html'), (error, data) => {
+          response.end(data);
+        })
+      }else if(error.errno === -4068) {
+        response.statusCode = 403;
+        response.end('无权访问目录')
+      }else {
+        response.statusCode = 500;
+        response.end('服务器繁忙')
+      }
+    }else {
+      response.end(data)
+    }
+  })
 });
 
 server.listen(8888);
