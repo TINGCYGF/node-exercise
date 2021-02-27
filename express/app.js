5601cca42a8bc63d1ad2cc1932be8106ef2017e4
TINGCYGF <1127163160@qq.com>
2021-02-27 09:53:31 +0800

    添加路由封装

--- express/app.js
+++ express/app.js
@@ -1,36 +1,22 @@
 const http = require('http');
-const path = require('path');
-const url = require('url');
 const routes = require('./module/routes');
-const ejs = require('ejs')
+const url = require('url');
+const path = require('path');
 
-
-http.createServer(function (req, res) {   
+http.createServer(function (req, res) {
     //创建静态web服务
-    routes.static(req,res,'static');
+    routes.static(req, res, 'static');
     //路由
-    let pathname=url.parse(req.url).pathname;
-
+    let pathname = url.parse(req.url).pathname.replace("/", "");
+    //http://127.0.0.1:3000/news      pathname=news
+    //http://127.0.0.1:3000/login      pathname=login
+    // http://127.0.0.1:3000/xxxx      pathname=xxxx   app.xxxx不存在
     let extname = path.extname(pathname);
-    if(!extname){ //如果有请求地址有后缀名的话让静态web服务去处理 
-        if(pathname==='/login'){
-            let msg = "数据库数据";
-            let list = [
-                {title: '新闻1'}, {title: '新闻2'}
-            ]
-            ejs.renderFile('./views/login.ejs', {msg: msg}, (err, data) => {
-                res.writeHead(200, { 'Content-Type': 'text/html;charset="utf-8"' });
-                res.end(data);
-            })
-        }else if(pathname==='/register'){
-            res.writeHead(200, { 'Content-Type': 'text/html;charset="utf-8"' });
-            res.end("执行注册");
-        }else if(pathname==='/admin'){
-            res.writeHead(200, { 'Content-Type': 'text/html;charset="utf-8"' });
-            res.end("处理后的业务逻辑");
-        }else{
-            res.writeHead(404, { 'Content-Type': 'text/html;charset="utf-8"' });
-            res.end("404");
+    if (!extname) {   //如果有后缀名的话让静态web服务去处理
+        try {
+            routes[pathname](req, res);
+        } catch (error) {
+            routes['error'](req, res);
         }
     }
 
